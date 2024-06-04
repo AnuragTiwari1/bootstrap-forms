7419d0f62c1cf4c9363ca08ead966b732917dab3
Sumit Kalyana <54006862+Sumitsrk25@users.noreply.github.com>
2024-06-04 14:01:58 +0530

    Bootstap Alignment & Classes added

--- src/App.tsx
+++ src/App.tsx
@@ -16,46 +16,60 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <div className="container">
-      <ul className="nav nav-tabs" id="myTab" role="tablist">
-        <li className={`nav-item ${activeTab === "traveler" ? "active" : ""}`}>
-          <button
-            className="nav-link active"
-            role="tab"
-            aria-controls="email-tab"
-            aria-selected={activeTab === "email"}
-            onClick={() => setActiveTab("email")}
-          >
-            Email Address
-          </button>
-        </li>
-        <li className="nav-item">
-          <button
-            className={`nav-link ${isEmailFormSubmitted ? "" : "disabled"} ${
-              activeTab === "traveler" ? "active" : ""
-            }`}
-            role="tab"
-            aria-controls="traveler-tab"
-            aria-selected={activeTab === "traveler"}
-            onClick={() => setActiveTab("traveler")}
-          >
-            Traveller(s) Info
-          </button>
-        </li>
-      </ul>
+    <div className="container mt-5 ">
+      <div className="row ">
+        <div className="col-md-10 bg-light offset-md-1">
+          <div className=" card border m-3  p-5">
+            <h4 className="text-primary mb-4">Book In 2 Simple Steps</h4>
+            <ul className="nav nav-tabs mb-5" id="myTab" role="tablist">
+              <li
+                className={`nav-item ${
+                  activeTab === "traveler" ? "active" : ""
+                }`}
+              >
+                <button
+                  className="nav-link active"
+                  role="tab"
+                  aria-controls="email-tab"
+                  aria-selected={activeTab === "email"}
+                  onClick={() => setActiveTab("email")}
+                >
+                  Email Address
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  className={`nav-link ${
+                    isEmailFormSubmitted ? "" : "disabled"
+                  } ${activeTab === "traveler" ? "active" : ""}`}
+                  role="tab"
+                  aria-controls="traveler-tab"
+                  aria-selected={activeTab === "traveler"}
+                  onClick={() => setActiveTab("traveler")}
+                >
+                  Traveller(s) Info
+                </button>
+              </li>
+            </ul>
 
-      <div className="tab-content">
-        <EmailForm
-          isActive={activeTab === "email"}
-          onSubmit={handleEmailFormSubmit}
-          onSubmittedValueChange={() => {
-            if (setIsEmailFormSubmitted) {
-              setIsEmailFormSubmitted(false);
-            }
-          }}
-        />
+            <div className="tab-content">
+              <EmailForm
+                isActive={activeTab === "email"}
+                onSubmit={handleEmailFormSubmit}
+                onSubmittedValueChange={() => {
+                  if (setIsEmailFormSubmitted) {
+                    setIsEmailFormSubmitted(false);
+                  }
+                }}
+              />
 
-        <TravelerForm isActive={activeTab === "traveler"} onSubmit={() => {}} />
+              <TravelerForm
+                isActive={activeTab === "traveler"}
+                onSubmit={() => {}}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
